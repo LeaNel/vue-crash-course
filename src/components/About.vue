d50d7c190945cc2dc4eb330f5457f5b85175a2c9
Leanne Nel <leanne@Leannes-MacBook-Air.local>
2018-02-19 14:39:23 +0200

    initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+      <h1>Hello there {{their_name}}, This is the about page</h1>
+      <p>Lorem itsum dolor met</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data() {
+        return {
+            their_name : this.$route.params.name
+        }
+    }
+  
+}
+</script>
+
